fde3c8a2592f178fc34365f30ccb8512e7475471
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-02-18 11:31:47 +0530

    GH Pages branch setup

--- 01-basics/bin/call.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-INGRESSGATEWAY=istio-ingressgateway
-IP_ADDRESS="$(minishift ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
-
-curl -H "Host: greeter.myproject.example.com" $IP_ADDRESS 
