4f9fd32d7205e809b6acb9d5ebeae0f1704c438f
Alan Conway <aconway@redhat.com>
2019-04-10 12:14:58 -0400

    Remove un-necessary INGRESSGATEWAY variables.
    Using a variable makes the commands longer and doesn't provide any benefit since:
    1. the variable name is almost as long as the value.
    2. it's only used a single time

--- 02-basics/bin/call.sh
+++ 02-basics/bin/call.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-INGRESSGATEWAY=istio-ingressgateway
-IP_ADDRESS="$(minishift ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
+IP_ADDRESS="$(minishift ip):$(kubectl get svc istio-ingressgateway --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
 
 curl -H "Host:greeter.knativetutorial.example.com" $IP_ADDRESS
