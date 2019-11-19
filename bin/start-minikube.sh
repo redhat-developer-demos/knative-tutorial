9a6d1b3a87da22a12470f4462248b9ea0c28b395
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-11-19 18:23:08 +0530

    updates for chapter 4

---
+++ bin/start-minikube.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+PROFILE_NAME=${PROFILE_NAME:-'knativetutorial'}
+MEMORY=${MEMORY:-8192}
+CPUS=${CPUS:-5}
+
+minikube profile $PROFILE_NAME
+
+minikube  --memory=$MEMORY --cpus=$CPUS \
+  --kubernetes-version=v1.14.0 \
+  --vm-driver=$VM_DRIVER \
+  --disk-size=50g \
+  --extra-config="apiserver.enable-admission-plugins=LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook" \
+  --insecure-registry='10.0.0.0/24' 
