801de0dfd2351d778b26e6c220c55961330e9ea7
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-11-25 22:05:23 +0530

    (chore) conflict merge

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -15,13 +15,7 @@ ServiceAccount,\
 DefaultStorageClass,\
 MutatingAdmissionWebhook"
 
-<<<<<<< HEAD
-minikube profile "$PROFILE_NAME"
-
-minikube start --memory="$MEMORY" --cpus="$CPUS" \
-=======
 minikube start --memory=$MEMORY --cpus=$CPUS \
->>>>>>> 99d0ef4783184bd533d0ab28d09a303989041abc
   --kubernetes-version=v1.14.0 \
   --vm-driver="$VM_DRIVER" \
   --disk-size=50g \
