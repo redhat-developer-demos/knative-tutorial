ea1d0a6b64080011a710fd78c835aa152e20ac88
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-10 16:45:35 +0530

    remove vm driver

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -3,7 +3,6 @@
 set -eu
 
 PROFILE_NAME=${PROFILE_NAME:-knativecookbook}
-VM_DRIVER=${VM_DRIVER:-virtualbox}
 MEMORY=${MEMORY:-8192}
 CPUS=${CPUS:-4}
 
@@ -19,7 +18,6 @@ MutatingAdmissionWebhook"
 minikube profile $PROFILE_NAME
 minikube start --memory=$MEMORY --cpus=$CPUS \
   --kubernetes-version=v1.15.0 \
-  --vm-driver="$VM_DRIVER" \
   --disk-size=50g \
   --extra-config="$EXTRA_CONFIG" \
   --insecure-registry='10.0.0.0/24' 
