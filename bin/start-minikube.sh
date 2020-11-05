55fd3d0f12af83c8c251d47fbcccda38571cc0bd
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-11-05 19:04:31 +0530

    (fixes) updates and fixes serving and install

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -9,6 +9,6 @@ CPUS=${CPUS:-6}
 minikube start -p $PROFILE_NAME \
   --memory=$MEMORY \
   --cpus=$CPUS \
-  --kubernetes-version=v1.18.0 \
+  --kubernetes-version=v1.19.0 \
   --disk-size=50g \
   --insecure-registry='10.0.0.0/24' 
