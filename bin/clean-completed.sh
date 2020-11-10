7da55f14ad84da622e3d5402853898d43c43f495
Ali Ok <aliok@redhat.com>
2020-11-10 17:54:43 +0300

    Knative Kafka related fixes

--- bin/clean-completed.sh
+++ bin/clean-completed.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Deleting completed pods"
 kubectl delete pods --field-selector=status.phase=Succeeded 
+
+echo "Deleting failed pods"
 kubectl delete pods --field-selector=status.phase=Failed
