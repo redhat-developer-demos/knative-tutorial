47c3ccf0a956a6027ad59d0a99fc28d7957b3bb8
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-11-10 12:31:48 +0530

    use hyperkit on Darwin

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -6,9 +6,22 @@ PROFILE_NAME=${PROFILE_NAME:-knativetutorial}
 MEMORY=${MEMORY:-8192}
 CPUS=${CPUS:-6}
 
-minikube start -p $PROFILE_NAME \
-  --memory=$MEMORY \
-  --cpus=$CPUS \
+unamestr=$(uname)
+
+if [ "$unamestr" == "Darwin" ];
+then
+  minikube start -p "$PROFILE_NAME" \
+  --memory="$MEMORY" \
+  --driver=hyperkit \
+  --cpus="$CPUS" \
   --kubernetes-version=v1.19.0 \
   --disk-size=50g \
   --insecure-registry='10.0.0.0/24' 
+else
+  minikube start -p "$PROFILE_NAME" \
+  --memory="$MEMORY" \
+  --cpus="$CPUS" \
+  --kubernetes-version=v1.19.0 \
+  --disk-size=50g \
+  --insecure-registry='10.0.0.0/24' 
+fi
