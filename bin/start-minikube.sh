8cca0e769981f0e1b96903fa47cb23960d098fab
ammbra <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>
2022-02-01 09:45:24 +0100

    Resolved comments from Sebastien.

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -12,7 +12,7 @@ if [ "$unamestr" == "Darwin" ];
 then
   minikube start -p "$PROFILE_NAME" \
   --memory="$MEMORY" \
-  --driver=virtualbox \
+  --driver=hyperkit \
   --cpus="$CPUS" \
   --kubernetes-version=v1.23.0 \
   --disk-size=50g \
